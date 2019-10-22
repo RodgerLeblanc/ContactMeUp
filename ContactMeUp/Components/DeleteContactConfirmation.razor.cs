6abc2ac36f4041af6038e028604f60280ec68473
Roger Leblanc <roger@rogerleblanc.net>
2019-10-22 00:02:25 -0400

    Create code behind files

--- ContactMeUp/Components/DeleteContactConfirmation.razor.cs
+++ ContactMeUp/Components/DeleteContactConfirmation.razor.cs
@@ -2,14 +2,11 @@
 using Blazored.Modal.Services;
 using ContactMeUp.Data;
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ContactMeUp.Components
 {
-    public class DeleteContactConfirmationBase : ComponentBase
+    public abstract class DeleteContactConfirmationBase : ComponentBase
     {
         [Inject] protected IModalService ModalService { get; set; }
         [Inject] protected ContactService ContactService { get; set; }
---
+++ ContactMeUp/Pages/ContactList.razor.cs
@@ -0,0 +1,45 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using ContactMeUp.Components;
+using ContactMeUp.Data;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContactMeUp.Pages
+{
+    public abstract class ContactListBase : ComponentBase
+    {
+        [Inject] protected IModalService ModalService { get; set; }
+        [Inject] protected ContactService ContactService { get; set; }
+        [Inject] protected NavigationManager NavigationManager { get; set; }
+
+        protected IList<Contact> Contacts { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Contacts = await ContactService.GetAsync();
+        }
+
+        protected void ConfirmDelete(Contact contact)
+        {
+            ModalService.OnClose += ModalClosed;
+
+            ModalParameters parameters = new ModalParameters();
+            parameters.Add("ToDelete", contact);
+
+            ModalService.Show<DeleteContactConfirmation>("Supprimer le contact?", parameters);
+        }
+
+        private async void ModalClosed(ModalResult modalResult)
+        {
+            ModalService.OnClose -= ModalClosed;
+
+            if (!modalResult.Cancelled)
+            {
+                Contacts = await ContactService.GetAsync();
+                StateHasChanged();
+            }
+        }
+    }
+}
